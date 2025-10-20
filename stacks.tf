cfb55fdffedcc7b802c1460a42a2a76a1e871a7b
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-20 18:15:23 -0400

    Remove stacks from configuration

--- stacks.tf
+++ stacks.tf
@@ -1,17 +1,19 @@
-resource "tfe_stack" "azure_core_infra" {
-  name       = "azure-core-infra"
-  project_id = tfe_project.azure.id
-  vcs_repo {
-    github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
-    identifier                 = "${var.github_organization}/azure-core-infra"
-  }
-}
+# 404 bug making these unusable - removed from state for now
 
-resource "tfe_stack" "azure_vault_hvd" {
-  name       = "azure-vault-hvd"
-  project_id = tfe_project.azure.id
-  vcs_repo {
-    github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
-    identifier                 = "${var.github_organization}/azure-vault-hvd"
-  }
-}
+# resource "tfe_stack" "azure_core_infra" {
+#   name       = "azure-core-infra"
+#   project_id = tfe_project.azure.id
+#   vcs_repo {
+#     github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
+#     identifier                 = "${var.github_organization}/azure-core-infra"
+#   }
+# }
+
+# resource "tfe_stack" "azure_vault_hvd" {
+#   name       = "azure-vault-hvd"
+#   project_id = tfe_project.azure.id
+#   vcs_repo {
+#     github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
+#     identifier                 = "${var.github_organization}/azure-vault-hvd"
+#   }
+# }
